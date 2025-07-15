59ff71d6062e58a82f5f2aa4942afc7c1cc98faf
Acecy237 <2112330017@e.gzhu.edu.cn>
2025-07-15 21:18:52 +0800

    new

--- RankAHuntTrainAssistant/MainWindow.cs
+++ RankAHuntTrainAssistant/MainWindow.cs
@@ -11,18 +11,29 @@ namespace RankAHuntTrainAssistant;
 
 public class MainWindow : Window, IDisposable
 {
-    private Plugin Plugin;
+
+    // 状态变量
+    private ushort currentMapId = 0;
 
     public MainWindow(Plugin plugin)
         : base("RankAHuntTrainAssistant##aht", ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse)
     {
-        Plugin = plugin;
     }
 
     public void Dispose() { }
 
     public override void Draw()
     {
+        ImGui.PushTextWrapPos();
+        ImGui.TextUnformatted("功能:自动寻路找A怪\n依赖插件: Vnavmesh、Teleporter");
+        ImGui.PopTextWrapPos();
+        ImGui.Separator();
 
+        if (ImGui.Button("测试地图ID"))
+        {
+            currentMapId = Svc.ClientState.TerritoryType;
+
+            Svc.Chat.Print($"当前地图ID: {currentMapId}");
+        }
     }
 }
